7740ba28073a1c9119eaf5c08b62b60c955c7a80
Camille Girodengo <115540569+Giro4c@users.noreply.github.com>
2025-09-05 12:54:13 +0200

    Starter Content + Project Setup

---
+++ Source/BeatAndRhythm/Private/RythmPlaygroundGameMode.cpp
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RythmPlaygroundGameMode.h"
+#include "RythmPlaygroundCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ARythmPlaygroundGameMode::ARythmPlaygroundGameMode()
+{
+	
+}
---
+++ Source/BeatAndRhythm/Public/RythmPlaygroundGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RythmPlaygroundGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARythmPlaygroundGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARythmPlaygroundGameMode();
+};
+
+
+
